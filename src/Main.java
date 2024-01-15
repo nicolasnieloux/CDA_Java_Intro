f12d875e0082bc6128f5529aae3367b40ce414f8
nicolasnieloux <nicolas.nieloux@le-campus-numerique.fr>
2024-01-15 11:15:17 +0100

    Refacto du code avec une entrée utilisateur

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,33 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
-    static void enumerate_while() {
+    static void enumerate_while(int inputUser) {
         int i = 1;
 
-        while (i <= 100) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+        while (i <= inputUser) {
             System.out.println("i = " + i);
             i++;
         }
     }
 
-    static void enumerate_for() {
-        for (int j = 1; j <= 100; j++) {
+    static void enumerate_for(int userNumber) {
+        for (int j = 1; j <= userNumber; j++) {
             System.out.println("j = " + j);
         }
     }
 
-    static void enumerate_even() {
-        for (int j = 0; j <= 100; j++) {
+    static void enumerate_even(int userNumber) {
+        for (int j = 0; j <= userNumber; j++) {
             if (j % 2 == 0) {
                 System.out.println("j pair = " + j);
             }
         }
     }
-    static void enumerate_odd() {
-        for (int j = 0; j <= 100; j++) {
+    static void enumerate_odd(int userNumber) {
+        for (int j = 0; j <= userNumber; j++) {
             if (j % 2 != 0) {
                 System.out.println("j impair = " + j);
             }
@@ -39,17 +39,21 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Exercice 1 - Suites de nombres simples");
 
+        Scanner inputUser = new Scanner(System.in);
+        System.out.println("Entrer un nombre entier");
+
+    int userNumber = inputUser.nextInt();
         // une boucle while, qui affiche tous les nombres de 1 à 100.
-        enumerate_while();
+        enumerate_while(userNumber);
 
         // une boucle for, qui affiche tous les nombres de 1 à 100.
-        enumerate_for();
+        enumerate_for(userNumber);
 
         // Affichage des nombres pairs
-        enumerate_even();
+        enumerate_even(userNumber);
 
         // Affichage des nombres impairs
-        enumerate_odd();
+        enumerate_odd(userNumber);
 
     }
 }
