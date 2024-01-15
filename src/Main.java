12756390512661bf5ec1467f41f64e6c87c0c801
nicolasnieloux <nicolas.nieloux@le-campus-numerique.fr>
2024-01-15 16:34:10 +0100

    Création du tableau et calcul du nombre de caractères

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,27 @@
+import java.util.ArrayList;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+    static void describe() {
+        ArrayList<String> bestFormateur = new ArrayList<String>();
+        bestFormateur.add("Jean");
+        bestFormateur.add("Ahmed");
+        bestFormateur.add("Lea");
+        bestFormateur.add("Blanca");
+
+        for (int i = 0; i < bestFormateur.size(); i++) {
+
+            int nombreCaracteres = bestFormateur.get(i).length();
+            System.out.println(("Nombre de caractères dans \"" + bestFormateur.get(i) + "\": " + nombreCaracteres));
+        }
+    }
+
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Traitement de chaînes de caractères");
+        describe();
     }
 }
