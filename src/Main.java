1ea7f3ff817798c79d1fcff8f29e860a473647f6
nicolasnieloux <nicolas.nieloux@le-campus-numerique.fr>
2024-01-15 11:05:38 +0100

    Réériture des programmes en utilisant des fonctions

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,8 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Exercice 1 - Suites de nombres simples");
 
-        // une boucle while, qui affiche tous les nombres de 1 à 100.
+    static void enumerate_while() {
         int i = 1;
 
         while (i <= 100) {
@@ -15,28 +11,46 @@ public class Main {
             System.out.println("i = " + i);
             i++;
         }
+    }
 
-
-        // une boucle for, qui affiche tous les nombres de 1 à 100.
-
+    static void enumerate_for() {
         for (int j = 1; j <= 100; j++) {
             System.out.println("j = " + j);
         }
+    }
 
-        // Affichage des nombres pairs
-
-        for (int j=0; j<=100; j++ ){
-            if (j%2 == 0){
+    static void enumerate_even() {
+        for (int j = 0; j <= 100; j++) {
+            if (j % 2 == 0) {
                 System.out.println("j pair = " + j);
             }
         }
-
-        // Affichage des nombres impairs
-
-        for (int j=0; j<=100; j++ ){
-            if (j%2 != 0){
+    }
+    static void enumerate_odd() {
+        for (int j = 0; j <= 100; j++) {
+            if (j % 2 != 0) {
                 System.out.println("j impair = " + j);
             }
         }
     }
+
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Exercice 1 - Suites de nombres simples");
+
+        // une boucle while, qui affiche tous les nombres de 1 à 100.
+        enumerate_while();
+
+        // une boucle for, qui affiche tous les nombres de 1 à 100.
+        enumerate_for();
+
+        // Affichage des nombres pairs
+        enumerate_even();
+
+        // Affichage des nombres impairs
+        enumerate_odd();
+
+    }
 }
+
