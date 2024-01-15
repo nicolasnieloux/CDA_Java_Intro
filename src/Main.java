4d42dba3ef733533c4f08f9cd162739966f2f45b
nicolasnieloux <nicolas.nieloux@le-campus-numerique.fr>
2024-01-15 12:23:32 +0100

    Suite de Fibonacci

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,35 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+    static int getInputUser() {
+        System.out.println("Exercice 2 - Suite de Fibonacci");
+
+        Scanner inputUser = new Scanner(System.in);
+        System.out.println("Entrer un nombre entier");
+
+        return inputUser.nextInt();
+    }
+
+    static ArrayList<Integer> getFibonacciArrayList(int inputUser) {
+        ArrayList<Integer> fibonacciArrayList = new ArrayList<>();
+        fibonacciArrayList.add(0);
+        fibonacciArrayList.add(1);
+        for (int i = 2; i < inputUser; i++) {
+            fibonacciArrayList.add(i, fibonacciArrayList.get(i - 2) + fibonacciArrayList.get(i - 1));
+        }
+        return fibonacciArrayList;
+    }
+
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Suite de Fibonacci!");
+        int inputUser = getInputUser();
+        System.out.println(getFibonacciArrayList(inputUser));
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
     }
 }
