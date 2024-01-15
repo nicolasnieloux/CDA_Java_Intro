1991fad2421e80ed258c958890bdb20485fce679
nicolasnieloux <nicolas.nieloux@le-campus-numerique.fr>
2024-01-15 11:40:57 +0100

    programme qui prend un nombre entier en paramètre et retourne le plus petit carré supérieur à n

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,30 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+import java.util.Scanner;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+    static int getInputUser() {
+        System.out.println("Exercice 1 - Suites de nombres simples");
+
+        Scanner inputUser = new Scanner(System.in);
+        System.out.println("Entrer un nombre entier");
+
+        return inputUser.nextInt();
+    }
+
+    static int findTheLowestInt(int number){
+        int lowestNumber = (int) Math.ceil(Math.sqrt(number));
+        return lowestNumber*lowestNumber;
+    }
+
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Exercice 2");
+        int intputUser = getInputUser();
+        int result = findTheLowestInt(intputUser);
+        System.out.println("Le plus petit carré est : " +result);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
     }
 }
+
