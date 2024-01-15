36e4020fcdd4c60f89e1376a39bf1cde6f867b08
nicolasnieloux <nicolas.nieloux@le-campus-numerique.fr>
2024-01-15 10:31:14 +0100

    Boucle for avec affichage des nombres pairs et impairs

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
         // une boucle while, qui affiche tous les nombres de 1 à 100.
         int i = 1;
 
-        while (i<=100) {
+        while (i <= 100) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
@@ -19,9 +19,24 @@ public class Main {
 
         // une boucle for, qui affiche tous les nombres de 1 à 100.
 
-        for (int j=1; j<=100; j++){
-            System.out.println("j = " +j);
+        for (int j = 1; j <= 100; j++) {
+            System.out.println("j = " + j);
         }
 
+        // Affichage des nombres pairs
+
+        for (int j=0; j<=100; j++ ){
+            if (j%2 == 0){
+                System.out.println("j pair = " + j);
+            }
+        }
+
+        // Affichage des nombres impairs
+
+        for (int j=0; j<=100; j++ ){
+            if (j%2 != 0){
+                System.out.println("j impair = " + j);
+            }
+        }
     }
 }
